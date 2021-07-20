b20ef26ce25bc8898806caf5dc80e52bfbd6b895
John Kelland <jak374@cornell.edu>
2021-07-19 19:20:41 -0500

    Forced unit tests to be recognized and resolved the netcore dll metadata error via csproj modification

---
+++ UnitTests/UserAccountUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Radancy_Bank_Challenge.Services;
+
+namespace Radancy_Bank_Challenge.UnitTests
+{
+    [TestClass]
+    public class UserAccountUnitTests
+    {
+        [TestMethod]
+        public void Setup()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
