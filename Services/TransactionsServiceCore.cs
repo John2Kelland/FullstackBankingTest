914441105e0897b5737095bad27269eb18c4ae66
John Kelland <john2kelland@gmail.com>
2021-07-19 22:06:44 -0500

    Added services to Transactions service core

--- Services/TransactionsServiceCore.cs
+++ Services/TransactionsServiceCore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Radancy_Bank_Challenge.Utilities;
 
 namespace Radancy_Bank_Challenge.Services
 {
@@ -9,13 +10,50 @@ namespace Radancy_Bank_Challenge.Services
     {
         #region Balance Modification Services
 
+        public bool ProcessDeposit(int accountID, int deposit)
+        {
+            bool transactionSuccessful = false;
 
+            if (!ValidateMaximumDeposit(deposit)) { return transactionSuccessful; }
 
+            // sql update logic to change Accounts.BALANCE based on Accounts.ACCOUNT_ID
+
+            return transactionSuccessful;
+        }
+
+        public bool ProcessWithdrawal(int accountID, int withdrawal)
+        {
+            bool transactionSuccessful = false;
+
+            // sql query logic to pull account balance from accountID
+            int accountBalance = 0;
+
+            if (!ValidateMaximumWithdrawal(accountBalance, withdrawal) ||
+                !ValidateMinimumAccountBalance(accountBalance - withdrawal)) { return transactionSuccessful; }
+
+            // sql update logic to change Accounts.BALANCE based on Accounts.ACCOUNT_ID
+
+            return transactionSuccessful;
+        }
+        
         #endregion
 
         #region Transaction Validation Rules
 
+        public bool ValidateMinimumAccountBalance(int remainingBalance)
+        {
+            return remainingBalance >= GlobalConstants.MinimumAccountBalance;
+        }
 
+        public bool ValidateMaximumWithdrawal(int accountBalance, int withdrawal)
+        {
+            return withdrawal <= GlobalConstants.MaximumWithdrawalPercentage*accountBalance;
+        }
+
+        public bool ValidateMaximumDeposit(int deposit)
+        {
+            return deposit <= GlobalConstants.MaximumDeposit;
+        }
 
         #endregion
     }
---
+++ Utilities/GlobalConstants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Radancy_Bank_Challenge.Utilities
+{
+    public class GlobalConstants
+    {
+        public static int MinimumAccountBalance {
+            get {
+                return 100;
+            }
+        }
+
+        public static double MaximumWithdrawalPercentage
+        {
+            get
+            {
+                return 0.9;
+            }
+        }
+
+        public static int MaximumDeposit
+        {
+            get
+            {
+                return 10000;
+            }
+        }
+    }
+}
